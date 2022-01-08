ee205680cf31b5db75089c5a6dec88d028e08b07
KosukeTaniguchi <kosuke@KosukeTaniguchinoMacBook-Air.local>
2022-01-08 11:30:40 +0900

    create dbConnect function

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -2,17 +2,28 @@ package contorollers
 
 import (
 	"net/http"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 )
 
+func dbConnect() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("cannot open the database")
+	}
+
+	return db
+}
+
+func getHome(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
+
 func StartWebServer() {
 	router := gin.Default()
 	router.LoadHTMLGlob("app/views/*")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"hello": "hello",
-		})
-	})
+	router.GET("/", getHome)
 	router.Run()
 }
