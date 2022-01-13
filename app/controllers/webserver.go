cb6938eaa05604364e0f87a1f60ae5300d1a3245
KosukeTaniguchi <kosuke@KosukeTaniguchinoMacBook-Air.local>
2022-01-13 15:14:04 +0900

    create getRequestUser func and fix createDocument func

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -26,7 +26,7 @@ func loginPage(c *gin.Context) {
 }
 
 func login(c *gin.Context) {
-	requestUser := getUser(c.PostForm("email"))
+	requestUser := getUserByEmail(c.PostForm("email"))
 
 	// DBからハッシュ化されたpasswordを取得
 	dbPassword := requestUser.Password
@@ -49,7 +49,7 @@ func login(c *gin.Context) {
 	}
 }
 
-func getUser(email string) models.User {
+func getUserByEmail(email string) models.User {
 	db := models.DbConnect()
 	var user models.User
 
@@ -129,12 +129,43 @@ func sessionCheck() gin.HandlerFunc {
 	}
 }
 
+func getRequestUser(c *gin.Context) models.User {
+	session := sessions.Default(c)
+	requestUserId := session.Get("UserId")
+
+	db := models.DbConnect()
+	var user *models.User
+	db.First(&user, "id = ?", requestUserId)
+	return *user
+}
+
 func createDocumentPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "create_document.html", gin.H{})
 }
 
 func createDocument(c *gin.Context) {
+	requestUser := getRequestUser(c)
+	db := models.DbConnect()
 
+	//DocumentのprimaryKeyのためのuuidを生成
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		log.Println(err)
+	}
+	id := uuid.String()
+	title := c.PostForm("title")
+	content := c.PostForm("content")
+
+	document := models.Document{
+		ID:       id,
+		Title:    title,
+		Content:  content,
+		AuthorID: requestUser.ID,
+		Author:   requestUser,
+	}
+	db.Create(&document)
+
+	c.Redirect(302, "/")
 }
 
 func StartWebServer() {
