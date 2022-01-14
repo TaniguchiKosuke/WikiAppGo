82fd8597962ac2eaff49c107e6a7d6033145717d
KosukeTaniguchi <kosuke@KosukeTaniguchinoMacBook-Air.local>
2022-01-14 22:27:42 +0900

    create getDocumentDetail func and document_detail.html

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -7,9 +7,9 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"gorm.io/gorm/clause"
 
 	"WikiAppGo/app/models"
+	"WikiAppGo/app/commons"
 )
 
 func getRequestUser(c *gin.Context) models.User {
@@ -24,17 +24,15 @@ func getRequestUser(c *gin.Context) models.User {
 
 func index(c *gin.Context) {
 	db := models.DbConnect()
-	var documents []models.Document
 	user := getRequestUser(c)
-	db.Preload(clause.Associations).Where("author_id", user.ID).Order("created_at desc").Limit(8).Find(&documents)
+	documents := commons.GetDocumentsList(user, db)
 	c.HTML(http.StatusOK, "index.html", gin.H{"documents": documents})
 }
 
 func createDocumentPage(c *gin.Context) {
 	db := models.DbConnect()
-	var documents []models.Document
 	user := getRequestUser(c)
-	db.Preload(clause.Associations).Where("author_id", user.ID).Order("created_at desc").Limit(8).Find(&documents)
+	documents := commons.GetDocumentsList(user, db)
 	c.HTML(http.StatusOK, "create_document.html", gin.H{"documents": documents})
 }
 
@@ -61,6 +59,25 @@ func createDocument(c *gin.Context) {
 	db.Create(&document)
 
 	c.Redirect(302, "/")
+}
+
+func getDocumentDetail(c *gin.Context) {
+	documentId := c.Param("id")
+	db := models.DbConnect()
+	var document models.Document
+	user := getRequestUser(c)
+	documents := commons.GetDocumentsList(user, db)
+	db.Where("id = ?", documentId).First(&document)
+	log.Println("############")
+	log.Println(document.Title, document.Content)
+
+	c.HTML(
+		http.StatusOK,
+		"document_detail.html",
+		gin.H{
+			"document": document,
+			"documents": documents,
+		})
 }
 
 func StartWebServer() {
